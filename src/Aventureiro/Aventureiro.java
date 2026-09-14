0d932d221d6afd240721917fba14899dfab8a248
Júlia de Medeiros Borba <borba.julia06@gmail.com>
2026-09-14 11:44:28 -0300

    importando tudo da main

--- src/Aventureiro/Aventureiro.java
+++ src/Aventureiro/Aventureiro.java
@@ -1,56 +1,137 @@
 package aventureiro;
 
-import java.util.ArrayList;
+/**
+ * @brief Classe Aventureiro, controla os atributos do personagem e compõe o próprio inventário.
+ * 
+ * @author Marcus Vinícius de Lima Souza
+ */
+
 import recompensa.Recompensa;
-import missao.Missao;
+import item.*;
+import inventario.*;
 
 public class Aventureiro {
-    private int pontos = 0;
-    private int energia_atual = 0;
-    private int energia = 0;
-    private int ataque = 0;
-    private int defesa = 0;
-    private int missoes_concluidas = 0;
-    private String nome = "";
-    private ArrayList<Missao> missoes = new ArrayList<>();
+    /// Atributos padrão do aventureiro
+    private boolean morto = false;          /// Determina se o aventureiro está vivo ou não
+    private int energia_maxima = 50;        /// Energia máxima do aventureiro
+    private int energia = 50;               /// Energia atual do aventureiro
+    private int ataque = 2;                 /// Ataque do aventureiro
+    private int defesa = 2;                 /// Defesa do aventureiro
+    private int missoes_concluidas = 0;     /// Número de missões concluidas pelo aventureiro
+    private String nome = "";               /// Nome do aventureiro
+    private Inventario bolsa;               /// Inventário do aventureiro
 
+    /// Construtor padrão
     public Aventureiro(String n) {
-        this.nome = n;
+        nome = n;
+        bolsa = new Inventario();
     }
 
-    public void restaurar_vida(int cura) {
-        energia_atual = Math.min(energia, energia_atual + cura);
+    /**
+     * Cura o aventureiro, evitando que o aventureiro tenha mais energia que a energia máxima limite
+     * ou que leve dano.
+     * 
+     * @param cura O valor pelo qual o aventureiro vai ser curado
+     */
+    public void curar(int cura) {
+        if (cura <= 0){
+            return;
+        }
+
+        energia = Math.min(energia_maxima, energia + cura);
     }
 
-    public void restaurar_vida(boolean total){
-        if (total){
-            energia_atual = energia;
+    /// Cura o aventureiro para sua vida total
+    public void restaurar_vida_total(){
+        energia = energia_maxima;
+    }
+
+    /**
+     * Dá dano ao aventureiro baseado em sua defesa, evitando que ele se cure ou que sua energia seja
+     * um número negativo.
+     * 
+     * @param dano O valor de dano infligido pelo inimigo antes de ser calculado com a defesa.
+     */
+    public void levar_dano(int dano){
+        if (dano <= 0){
+            return;
+        }
+
+        double danoreal;
+
+        if (defesa > 0 && defesa < 10){
+            danoreal = dano * (10.0 / defesa);
+        } else {
+            danoreal = dano;
+        }
+
+        energia = Math.max(0, energia - (int) danoreal);
+
+        if (energia == 0) { morreu(); }
+    }
+
+    /// Aumenta o número de missões concluidas
+    public void concluir_missao() {
+        missoes_concluidas++;
+    }
+
+    /// Muda morto para true se o personagem tiver morrido
+    public void morreu(){
+        morto = true;
+    }
+
+    /**
+     * Atualiza os atributos do aventureiro e adiciona a recompensa ao inventário caso possivel.
+     * 
+     * @param recomp A recompensa a ser recebida.
+     */
+    public void ganhar_recompensa(Recompensa recomp) {
+        System.out.print("Parabéns! Você ganhou um(a)");
+        System.out.print(recomp.getDescricao());
+        System.out.print('\n');
+
+        if (recomp.getTipo() == Item.TipoItem.CONSUMIVEL || recomp.getTipo() == Item.TipoItem.CONSUMIVELUNICO){
+            System.out.print("HP ");
+            System.out.print(energia_maxima);
+            System.out.print(" -> ");
+            energia_maxima += recomp.getValor();
+            System.out.println(energia_maxima);
+        }
+
+        if (recomp.getTipo() == Item.TipoItem.ARMA){
+            System.out.print("ATQ ");
+            System.out.print(ataque);
+            System.out.print(" -> ");
+            ataque += recomp.getValor();
+            System.out.println(ataque);
+        }
+
+        if (recomp.getTipo() == Item.TipoItem.ARMADURA){
+            System.out.print("DEF ");
+            System.out.print(defesa);
+            System.out.print(" -> ");
+            defesa += recomp.getValor();
+            System.out.println(defesa);
+        }
+
+        if (recomp.getTipo() == Item.TipoItem.CONSUMIVELUNICO){
+            System.out.println("Você consumiu a/o " + recomp.getDescricao());
+        } else {
+            if (bolsa.addItem(recomp)){
+                System.out.println(recomp.getDescricao() + " foi adicionado(a) ao seu inventário!");
+            } else {
+                System.out.println(recomp.getDescricao() + " não foi adicionado(a) ao seu inventário pois seu inventário está cheio!");
+                System.out.println("Você ainda pode desfrutar das melhorias do item mesmo assim!");
+            }
         }
     }
 
-    public void Levar_dano(int dano){
-        energia_atual = Math.max(0, energia_atual - dano);
-    }
-
-    public void concluir_missao() {
-        this.missoes_concluidas++;
-    }
-
-    public void morreu(){
-        this.missoes_concluidas = -1;
-    }
-
-    public void ganhar_recompensa(Recompensa recomp) {
-        this.energia += recomp.getEnergia();
-        this.ataque += recomp.getAtaque();
-        this.defesa += recomp.getDefesa();
-    }
-
     // Getters 
-    public int get_pontos() { return pontos; }
-    public String get_nome() { return nome; }
-    public int get_energia() { return energia; }
-    public int get_ataque() { return ataque; }
-    public int get_defesa() { return defesa; }
-    public int get_missoes_concluidas() { return missoes_concluidas; }
+    public boolean morto()             { return morto;              }
+    public String getNome()            { return nome;               }
+    public int getEnergia()            { return energia;            }
+    public int getAtaque()             { return ataque;             }
+    public int getDefesa()             { return defesa;             }
+    public int getMissoesConcluidas()  { return missoes_concluidas; }
+    public Inventario getInventario()  { return bolsa;              }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,161 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+
+/**
+ * @brief Programa principal, responsável por juntar todas as classes e atributos.
+ * 
+ * @author Matheus Lucheze Lopes
+ */
 
 import aventureiro.Aventureiro;
+import item.Item;
 import missao.Missao;
+import missao.Missao.Estado;
 import recompensa.Recompensa;
 
 public class Main {
-    public int loop_luta(Aventureiro p, Missao m, Scanner scanner_legal){
-        //retorna 0 se ganhar, 1 se perder e 2 se fugir
-        p.restaurar_vida(true);
 
-        while (!m.obito()){
-            if (p.get_energia() <= 0){
-                return 1;
-            }
-
-            char op = scanner_legal.next().charAt(0);
-
-            // Se a -> ataque, r -> run, i -> acessa o inventario de consumiveis
-            if (op == 'a' || op == 'A'){
-                m.Levar_dano( p.get_ataque() );
-                p.Levar_dano( m.get_ataque() );
-            }
-        }
-        
-        return 0;
-    }
-
-    public static void main(String[] Args) {
+    /// Programa principal
+    public static void main(String[] args) {
         Aventureiro p = new Aventureiro("Caipora");
         Scanner scanner_legal = new Scanner(System.in);
 
-        while (p.get_missoes_concluidas() != -1 && p.get_missoes_concluidas() != 3){
-            int resultado = -1;
+        p.restaurar_vida_total();
 
-            if (p.get_missoes_concluidas() == 0){
-                Recompensa rw = new Recompensa("ha", 1, 1, 1);
-                Missao inimigo1 = new Missao("Slime", 1, 2, 10, rw);
+        System.out.println("=============== COMBAT QUEST ===============");
+        System.out.println("Aventureiro: " + p.getNome());
 
-                resultado = loop_luta(p, inimigo1, scanner_legal);
-            } else if (p.get_missoes_concluidas() == 1) {
-                Recompensa rw = new Recompensa("ha", 1, 1, 1)
-                Missao inimigo2 = new Missao("Goblin", 2, 5, 40, rw)
-                
-            } else if (p.get_missoes_concluidas() == 2) {
-                Recompensa rw = new Recompensa("ha", 1, 1, 1)
-                Missao inimigo3 = new Missao("Ogro", 4, 7, 100, rw)
+        while (!p.morto() && p.getMissoesConcluidas() < 3) {
+            p.restaurar_vida_total();
+            Missao inimigoAtual = null;
+
+            if (p.getMissoesConcluidas() == 0) {
+                Recompensa rw = new Recompensa("Poção Pequena", Item.TipoItem.CONSUMIVELUNICO, Item.Raridade.COMUM, 100, 1);
+                inimigoAtual = new Missao("Slime", 1, 2, 80, rw);
+                loop_luta(p, inimigoAtual, scanner_legal);
+
+            } else if (p.getMissoesConcluidas() == 1) {
+                Recompensa rw = new Recompensa("Espada Sombria", Item.TipoItem.ARMA, Item.Raridade.RARO, 20, 2);
+                inimigoAtual = new Missao("Goblin", 1, 5, 100, rw);
+                loop_luta(p, inimigoAtual, scanner_legal);
+
+            } else if (p.getMissoesConcluidas() == 2) {
+                Recompensa rw = new Recompensa("Capa Carmesim", Item.TipoItem.ARMADURA, Item.Raridade.EPICO, 5, 3);
+                inimigoAtual = new Missao("Vampiro", 2, 8, 350, rw);
+                loop_luta(p, inimigoAtual, scanner_legal);
             }
 
-            if (resultado == 0){
-                p.ganhar_recompensa();
+            if (inimigoAtual.getEstado() == Estado.CONCLUIDA) {
+                System.out.println("\nBoa " + p.getNome() + "! Você detonou nessa missão!");
+                p.ganhar_recompensa(inimigoAtual.getRecompensa());
                 p.concluir_missao();
-            } else if (resultado == 1) {
+            } else if (inimigoAtual.getEstado() == Estado.FALHA) {
                 p.morreu();
-            } else if (resultado == 2){
-                //todo run method
+                System.out.println("\nVocê foi derrotado(a)...");
+                break;
+            } else if (inimigoAtual.getEstado() == Estado.FUGA) {
+                System.out.println("\nFugiu da batalha. Descanse um pouco antes de atacar novamente.");
+                p.restaurar_vida_total();
             }
+
+            inimigoAtual.ResetaMissão();
         }
 
-    } 
+        if (p.getMissoesConcluidas() == 3) {
+            System.out.println("\nParabéns! Você é um(a) verdadeiro(a) guerreiro(a)!");
+        } else {
+            System.out.println("\nQue pena! Mais sorte próxima vez guerreiro(a).");
+        }
+
+        scanner_legal.close();
+    }
+
+    /**
+     * Loop do jogo que cuida do progresso de uma missao.
+     * 
+     * @param p O personagem do jogador
+     * @param inimigo Os dados da missão e do inimigo que está sendo enfrentado
+     * @param scanner_legal Scanner para ler as entradas do usuário e decidir o que acontece no jogo
+     */
+    public static void loop_luta(Aventureiro p, Missao inimigo, Scanner scanner_legal) {
+        System.out.println("\n============================================");
+        System.out.println("Lute contra: " + inimigo.getNome());
+
+        inimigo.AceitarMissao();
+
+        while (inimigo.getEstado() == Estado.EM_PROGRESSO) {
+            System.out.println("\n[Status " + p.getNome() + "] HP: " + p.getEnergia() + " | ATQ: " + p.getAtaque() + " | DEF: " + p.getDefesa());
+            System.out.println("[Status " + inimigo.getNome() + "] HP: " + inimigo.getEnergia() + " | ATQ: " + inimigo.getAtaque());
+            System.out.println("Escolha sua ação: ");
+            System.out.println("1. Atacar");
+            System.out.println("2. Fugir");
+
+            String acao = scanner_legal.nextLine();
+
+            if (acao.equals("1")) {
+                int temp = calcula_dano(p.getAtaque(), inimigo.getDefesa());
+
+                System.out.println("\n" + p.getNome() + " ataca!");
+                System.out.println(p.getNome() + " causa " + temp + " de dano!");
+
+                inimigo.LevarDano(p.getAtaque());
+
+                if (inimigo.Obito()) {
+                    inimigo.VitoriaJogador();
+                    return;
+                }
+
+                temp = calcula_dano(inimigo.getAtaque(), p.getDefesa());
+
+                System.out.println(inimigo.getNome() + " contra-ataca e causa " + temp + " de dano!");
+                p.levar_dano(inimigo.getAtaque());
+
+                if (p.morto()) {
+                    inimigo.VitoriaInimiga();
+                    return;
+                }
+
+            } else if (acao.equals("2")) {
+                inimigo.Fugir();
+                return;
+            } else {
+                System.out.println("Ação inválida! Você perdeu o turno.");
+
+                int temp = calcula_dano(inimigo.getAtaque(), p.getDefesa());
+
+                System.out.println(inimigo.getNome() + " ataca e causa " + temp + " de dano!");
+                p.levar_dano(inimigo.getAtaque());
+
+                if (p.morto()) {
+                    inimigo.VitoriaInimiga();
+                    return;
+                }
+            }
+        }
+    }
+
+    /**
+     * Função que calcula o dano que será levado por uma entidade baseado na sua própria defesa
+     * e no ataque de quem está inflingindo o dano
+     * 
+     * @param ataque O valor do ataque da entidade que está atacando
+     * @param defesa O valor da defesa de quem está sendo atacado
+     * 
+     * @return O valor calculado de quanto dano foi causado.
+     */
+    public static int calcula_dano(int ataque, int defesa){
+        if (ataque <= 0){
+            return 0;
+        }
+
+        double danoreal;
+
+        if (defesa > 0 && defesa < 10){
+            danoreal = ataque * (10.0 / defesa);
+        } else {
+            danoreal = ataque;
+        }
+
+        return (int) danoreal;
+    }
 }
+
